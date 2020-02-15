5eaaf6b61508500a0348c254ce96832235cfb724
Shreyapothkanoori <shreyapothkanoori@gmail.com>
2020-02-16 01:12:05 +0530

    adding the project

---
+++ src/test/java/maven_demo2/Caltest.java
@@ -0,0 +1,14 @@
+package maven_demo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Caltest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
